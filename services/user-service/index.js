2e8105eaec754a5f460ededd40cdddeaad9b5498
Pasindu Viraj <pasinduviraj2399@gmail.com>
2026-02-09 07:31:14 +0530

    feat: configure environment variables for order, payment, and user services

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const { Pool } = require("pg");
+require("dotenv").config();
+
 const app = express();
 app.use(express.json());
 
 const pool = new Pool({
-  host: "localhost",
-  user: "admin",
-  password: "admin",
-  database: "platformdb",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 app.get("/health", (req, res) => {
